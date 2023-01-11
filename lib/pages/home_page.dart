443403165916fa01bb915ef26eb554ec83b8c96f
Pedro Meerholz <developer.pedromeerholz@gmail.com>
2023-01-11 13:48:53 -0300

    Adicionando o botão principal e colocando os componentes dentro de um ScrollView

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -23,46 +23,64 @@ class HomePageState extends State<HomePage> {
           ],
         ),
         backgroundColor: Colors.white,
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: const [
-              Icon(
-                Icons.person,
-                size: 120,
-                color: Colors.lightGreen,
-              ),
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Peso (Kg)',
-                  labelStyle: TextStyle(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Icon(
+                  Icons.person,
+                  size: 120,
+                  color: Colors.lightGreen,
+                ),
+                const TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Peso (Kg)',
+                    labelStyle: TextStyle(
+                      color: Colors.lightGreen,
+                    ),
+                  ),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
                     color: Colors.lightGreen,
+                    fontSize: 20,
                   ),
                 ),
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.lightGreen,
-                  fontSize: 20,
-                ),
-              ),
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Altura (cm)',
-                  labelStyle: TextStyle(
+                const TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Altura (cm)',
+                    labelStyle: TextStyle(
+                      color: Colors.lightGreen,
+                    ),
+                  ),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
                     color: Colors.lightGreen,
+                    fontSize: 20,
                   ),
                 ),
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.lightGreen,
-                  fontSize: 20,
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 10,
+                    bottom: 10,
+                  ),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Calcular'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.lightGreen,
+                    ),
+                  ),
                 ),
-              ),
-            ],
+                Text(
+                  'Teste',
+                ),
+              ],
+            ),
           ),
         ),
       ),
