27022f6d45ca7f6673b4d55b28fd77b8be1046b9
Pedro Meerholz <developer.pedromeerholz@gmail.com>
2023-01-11 13:40:39 -0300

    Adicionando o ícone principal e os campos de texto para entrada das informações

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -22,6 +22,49 @@ class HomePageState extends State<HomePage> {
             ),
           ],
         ),
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: const [
+              Icon(
+                Icons.person,
+                size: 120,
+                color: Colors.lightGreen,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Peso (Kg)',
+                  labelStyle: TextStyle(
+                    color: Colors.lightGreen,
+                  ),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.lightGreen,
+                  fontSize: 20,
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Altura (cm)',
+                  labelStyle: TextStyle(
+                    color: Colors.lightGreen,
+                  ),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.lightGreen,
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
